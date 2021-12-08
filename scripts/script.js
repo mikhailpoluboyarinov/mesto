80cd83d3f052ffbd40c39b160e104f93f9fd648d
Mikhail <mikhailpoluboyarinov@yandex.ru>
2021-12-08 04:16:09 +0300

    Стилизация проекта, добавление интерактивности

---
+++ scripts/script.js
@@ -0,0 +1,29 @@
+let editPopup = document.querySelector('.profile__edit-button');
+let closeForm = document.querySelector('.form__close-button');
+let popup = document.querySelector('.popup');
+let formElement = document.querySelector('.form');
+let nameInput = document.querySelector('.form__input-name');
+let jobInput = document.querySelector('.form__input-profession');
+let userName = document.querySelector('.profile__title');
+let userJob = document.querySelector('.profile__subtitle');
+
+function openPopup() {
+    popup.classList.add('popup_opened');
+    nameInput.value = userName.textContent;
+    jobInput.value = userJob.textContent;
+}
+
+function closePopup() {
+    popup.classList.remove('popup_opened');
+}
+
+function formSubmitHandler (evt) {
+    evt.preventDefault();
+    userName.textContent = nameInput.value;
+    userJob.textContent = jobInput.value;
+    closePopup();
+}
+
+editPopup.addEventListener('click', openPopup);
+closeForm.addEventListener('click', closePopup);
+formElement.addEventListener('submit', formSubmitHandler);
